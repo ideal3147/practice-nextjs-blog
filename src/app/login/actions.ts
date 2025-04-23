3e7959cd62c2ac5df3ee2d57034885999a85e4fe
ideal3147 <67406457+ideal3147@users.noreply.github.com>
2025-04-23 22:43:23 +0900

    feat: block sign up (#15)

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -25,22 +25,27 @@ export async function login(formData: FormData) {
   redirect('/')
 }
 
-export async function signup(formData: FormData) {
-  const supabase = await createClient()
+// export async function signup(formData: FormData) {
+  
+//   const supabase = await createClient()
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  }
+//   // type-casting here for convenience
+//   // in practice, you should validate your inputs
+//   const data = {
+//     email: formData.get('email') as string,
+//     password: formData.get('password') as string,
+//   }
 
-  const { error } = await supabase.auth.signUp(data)
+//   const { error } = await supabase.auth.signUp(data)
 
-  if (error) {
+//   if (error) {
+//     redirect('/error')
+//   }
+
+//   revalidatePath('/', 'layout')
+//   redirect('/account')
+// }
+
+export async function signup() {
     redirect('/error')
-  }
-
-  revalidatePath('/', 'layout')
-  redirect('/account')
 }
